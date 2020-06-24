6ebb141798cc0887f8a010fab2c1b726b04e6b2d
hudsone8024 <67377906+hudsone8024@users.noreply.github.com>
2020-06-24 10:21:22 -0400

    Add files via upload
    First Submission

---
+++ P2T1_SalesPrediction_HudsonEric.py
@@ -0,0 +1,13 @@
+# This Project is to display Sales prediction.
+print ('06/23/2020')
+print ('CTI-110 P2T1 - Sales Prediction')
+print ('Eric Hudson')
+#
+# Get the projected total sales.
+total_sales= float(input('Enter the projected sales:'))
+
+# Calculate the profit as 23 percent of the total sales.
+profit= total_sales * 0.23
+
+# Display the profit.
+print ( 'The profit is $', profit)
