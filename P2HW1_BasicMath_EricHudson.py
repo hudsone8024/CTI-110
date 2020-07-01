3cc5a4eaa66f3a83a7fc1c58d8ef390a3b8134f5
hudsone8024 <67377906+hudsone8024@users.noreply.github.com>
2020-07-01 00:21:42 -0400

    Add files via upload

---
+++ P2HW1_BasicMath_EricHudson.py
@@ -0,0 +1,28 @@
+# Solving Basic Math
+# 06/23/2020
+# CTI-110 P2HW1- Basic Math
+# Eric Hudson
+#
+# Get first number.
+value1 = float(input('Enter first number:'))
+
+# Get second number.
+value2 = float(input('Enter second number:'))
+
+# Get sum of two numbers.
+sum = value1 + value2
+
+# Get result of Multiplying the two numbers.
+total_value = value1 * value2
+
+
+
+# First Number Entered
+print ("First number entered: ", value1)
+# Second Number Enetered
+print ("Second number entered: ", value2)
+# Sum of both numbers
+print ("Sum of both numbers: ", sum)
+# Result of Multiplying the two numbers
+print ("Result of Multiplying the two numbers: ", total_value)
+
---
+++ P2HW2_TurtleGraphic_HudsonEric.py
@@ -0,0 +1,28 @@
+# Creating a turtle graphic
+print ('06/23/2020')
+print ('CTI-110 P@HW2- Turtle Graphic')
+print ('Eric Hudson')
+#
+#
+>>> import turtle
+>>> turtle.showturtle()
+>>> turtle.circle(25)
+>>> turtle.left(90)
+>>> turtle.forward(100)
+>>> turtle.left(180)
+>>> turtle.forward(50)
+>>> turtle.forward(25)
+>>> turtle.right(90)
+>>> turtle.forward(50)
+>>> turtle.forward(10)
+>>> turtle.forward(10)
+>>> turtle.right(180)
+>>> turtle.forward(150)
+>>> turtle.left(180)
+>>> turtle.foward(70)
+>>> turtle.forward(70)
+>>> turtle.forward(10)
+>>> turtle.left(90)
+>>> turtle.forward(100)
+>>> turtle.left(180)
+>>> turtle.forward
